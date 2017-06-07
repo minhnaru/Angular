86f169ff92a8bb08adb5f62e5a4ba578779b4c6d
Minh Nguyen <nguyenminhnaru@gmail.com>
2017-06-07 11:21:31 +0700

    add

--- s1.js
+++ s1.js
@@ -5,4 +5,43 @@ app.controller('myCtrl', function($scope) {
 	$scope.lastname = "Naru";
 });
 
-// 3
+// 4
+app.controller('myCtrl4', function($scope) {
+	$scope.name_4 = "Click here";
+	$scope.changeName = function() {
+		$scope.name_4 = "Hello Minh	";
+	}
+});
+
+// 5
+app.controller('myCtrl5', function($scope) {
+	$scope.firstname5 = "John";
+	$scope.lastname5 = "Doe";
+	$scope.fullName = function() {
+		return $scope.firstname5 + " " + $scope.lastname5;
+	};
+})
+
+// 6
+app.controller('myCtrl6', function($scope) {
+	$scope.name6 = [
+		{name:'Jani', country:'Norway'},
+		{name:'Hege', country:'Sweden'},
+		{name:'Kaik', country:'Denmark'}
+	];
+})
+
+// 7
+app.controller('myCtrl7', function($scope) {
+	$scope.name7 = [
+		'Jani',
+		'Carl',
+        'Margareth',
+        'Hege',
+        'Joe',
+        'Gustav',
+        'Birgit',
+        'Mary',
+        'Kai'
+	];
+});
