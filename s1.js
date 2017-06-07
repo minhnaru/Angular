e60c5cd2a84172d97dcff3d75479066fbe152ed4
Minh Nguyen <nguyenminhnaru@gmail.com>
2017-06-07 13:45:36 +0700

    add

--- s1.js
+++ s1.js
@@ -45,3 +45,41 @@ app.controller('myCtrl7', function($scope) {
         'Kai'
 	];
 });
+
+// 8
+app.controller('myCtrl8', function($scope) {
+	$scope.name8 = [
+		'Jani',
+        'Carl',
+        'Margareth',
+        'Hege',
+        'Joe',
+        'Gustav',
+        'Birgit',
+        'Mary',
+        'Kai'
+	];
+});
+
+// 9
+app.controller('myCtrl9', function($scope) {
+	$scope.name9 = [
+		{name:'Jani',country:'Norway'},
+        {name:'Carl',country:'Sweden'},
+        {name:'Margareth',country:'England'},
+        {name:'Hege',country:'Norway'},
+        {name:'Joe',country:'Denmark'},
+        {name:'Gustav',country:'Sweden'},
+        {name:'Birgit',country:'Denmark'},
+        {name:'Mary',country:'England'},
+        {name:'Kai',country:'Norway'}
+	];
+	$scope.orderByMe = function(x) {
+		$scope.myOrderBy = x;
+	}
+});
+
+// 10
+app.controller('myCtrl10', function($scope, $location) {
+	$scope.myUrl = $location.absUrl();
+});
